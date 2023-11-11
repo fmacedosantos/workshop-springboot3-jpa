f3296d811751aefd82dc559dd4de747d0aa9e048
Felipe Macedo dos Santos <89111798+Maced40@users.noreply.github.com>
2023-11-11 17:47:02 -0300

    Project created

---
+++ src/main/java/com/fs_maced0/courseSpringBoot/CourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fs_maced0.courseSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fs_maced0/courseSpringBoot/CourseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs_maced0.courseSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
